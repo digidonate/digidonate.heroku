537e0c8d032cc87e495e6be2776f287224f4d0eb
unknown <ju3tin@hotmil.co.uk>
2019-11-14 05:03:20 +0000

    update

--- config/config.php
+++ config/config.php
@@ -1,16 +1,7 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password)fdfggfg */
-define('DB_SERVER', 'lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306');
-define('DB_USERNAME', 'bisz6nf2u5ymifre');
-define('DB_PASSWORD', 'ufcnqnkte0ofavy8');
+# In "config.php"
+# DB configuration
 define('DB_NAME', 'fizn07ewny2rctav');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?> 
+define('DB_USER', 'bisz6nf2u5ymifre');
+define('DB_PASSWORD', 'ufcnqnkte0ofavy8');
+define('DB_HOST', 'lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306');
