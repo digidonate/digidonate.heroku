8a7f4c89c5e5d7b060de3c2f152dea65e28d675b
unknown <ju3tin@hotmil.co.uk>
2019-11-14 05:04:51 +0000

    update

--- config/config.php
+++ config/config.php
@@ -1,7 +1,18 @@
 <?php
-# In "config.php"
-# DB configuration
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password)fdfggfg */
 define('DB_NAME', 'fizn07ewny2rctav');
 define('DB_USER', 'bisz6nf2u5ymifre');
 define('DB_PASSWORD', 'ufcnqnkte0ofavy8');
 define('DB_HOST', 'lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?> 
+
+
